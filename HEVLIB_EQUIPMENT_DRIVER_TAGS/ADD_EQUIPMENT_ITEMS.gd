2317d53cb60e83c0a95736899b4244538d1640d0
JCalebBR <josshuacaleb@gmail.com>
2026-06-13 01:44:23 -0300

    Add missing files for hud

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/ADD_EQUIPMENT_ITEMS.gd
@@ -0,0 +1,10 @@
+extends Node
+
+const RINGA_CERF_HUD = {
+	"system":"SYSTEM_RINGA_CERF_HUD",
+	"manual":"SYSTEM_RINGA_CERF_HUD_MANUAL",
+	"test_protocol":"hud",
+	"slot_type":"HUD",
+	"equipment_type":"COMPUTER_HUD",
+	"price":1
+}
